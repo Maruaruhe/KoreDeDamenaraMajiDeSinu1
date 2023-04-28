7bb3db792b236e6be38d3931192df9e13c8e8574
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-04-28 11:30:54 +0900

    プロジェクト ファイルを追加します。

---
+++ Matrix.h
@@ -0,0 +1,20 @@
+#pragma once
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+static const int kColumnWidth = 60;
+static const int kRowHeight = 20;
+
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 Inverse(const Matrix4x4& m);
+Matrix4x4 Transpose(const Matrix4x4& m);
+Matrix4x4 MakeIdentity4x4();
+
+float InverseNum(const Matrix4x4& m, int a, int b, int c, int d, int e, int f, int g, int h);
+float InverseNum2(const Matrix4x4& m, int a, int b, int c, int d, int e, int f);
+
+void MatrixScreenPrint(int x, int y, Matrix4x4& m);
