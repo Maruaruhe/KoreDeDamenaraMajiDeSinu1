18b12909b6e1e82f3022080d8cda12cd467466eb
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2023-07-07 11:00:57 +0900

    kj

--- GridSphere.cpp
+++ GridSphere.cpp
@@ -11,7 +11,7 @@ void DrawGrid(const Matrix4x4& worldViewProjectionMatrix, const Matrix4x4& viewp
 	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex) {
 		Vector3 screenVertices[11];
 		Vector3 kLocalVertices[11];
-		kLocalVertices[xIndex] = {0.0f + xIndex * kGridHalfWidth,0.0f,0.0f};
+		kLocalVertices[xIndex] = {1.0f + xIndex * kGridHalfWidth,1.0f + xIndex * kGridHalfWidth,0.0f};
 		Vector3 ndcVertex = Transform(kLocalVertices[xIndex], worldViewProjectionMatrix);
 		screenVertices[xIndex] = Transform(ndcVertex, viewportMatrix);
 		Novice::DrawLine(screenVertices[xIndex].x, screenVertices[xIndex].y + xIndex * kGridEvery, screenVertices[xIndex].x + kGridEvery, screenVertices[xIndex].y + xIndex * kGridEvery, 0xAAAAAAFF);
